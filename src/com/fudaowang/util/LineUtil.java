ba201d94a609dbeefb83a34ff90063567819b1cf
zantesu <dongxin@live.com>
2012-11-09 00:04:09 +0800

    完善空对象的异常处理

--- src/com/fudaowang/util/LineUtil.java
+++ src/com/fudaowang/util/LineUtil.java
@@ -734,4 +734,8 @@ public class LineUtil {
     public static boolean linesIntersect(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
         return java.awt.geom.Line2D.linesIntersect(x1, y1, x2, y2, x3, y3, x4, y4);
     }
+
+    public static boolean linesIntersect(Segment segment, Line line) {
+
+    }
 }
