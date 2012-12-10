24b41e5d7b638bf4fb1dfc0a9aec3feb2ba5a6ae
zantesu <dongxin@live.com>
2012-12-10 10:10:39 +0800

    添加对多边形重合的判定方法

--- src/com/fudaowang/common/util/PolygonUtil.java
+++ src/com/fudaowang/common/util/PolygonUtil.java
@@ -4,6 +4,8 @@ import com.fudaowang.common.graph.Line;
 import com.fudaowang.common.graph.Point;
 import com.fudaowang.common.graph.Polygon;
 import com.fudaowang.common.graph.Segment;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
 import org.apache.commons.collections.Transformer;
 
 /**
@@ -119,4 +121,68 @@ public class PolygonUtil {
         return new Polygon(result);
     }
 
+    /**
+     * 判断两个多边形是否每个点都重合
+     *
+     * @param source    第一个多边形
+     * @param target    第二个多边形
+     * @param precision 给定的精度
+     * @return 若两个多边形点集完全重合, 则返回true
+     */
+    public static boolean coincide(Polygon source, Polygon target, double precision) {
+        if (source == null || target == null) {
+            throw new NullPointerException("多边形为null");
+        }
+        return coincide(source.getPoints(), target.getPoints(), precision);
+    }
+
+    /**
+     * 在最小精度范围内判断两个多边形是否每个点都重合
+     *
+     * @param source 第一个多边形
+     * @param target 第二个多边形
+     * @return 若两个多边形点集完全重合, 则返回true
+     */
+    public static boolean coincide(Polygon source, Polygon target) {
+        return coincide(source, target, NumberUtil.MIN_VALUE);
+    }
+
+    /**
+     * 判断点集是否每个点都重合
+     *
+     * @param source    第一个点集
+     * @param target    第二个点集
+     * @param precision 给定的精度
+     * @return 若两个点集完全重合, 则返回true
+     */
+    public static boolean coincide(Point[] source, final Point[] target, final double precision) {
+        if (source == null || target == null) {
+            throw new NullPointerException("点集为null");
+        }
+        if (source.length != target.length) {
+            return false;
+        }
+        return PointUtil.exists(source, new Predicate() {
+            public boolean evaluate(Object o) {
+                final Point p = (Point) o;
+                return PointUtil.exists(target, new Predicate() {
+                    public boolean evaluate(Object o) {
+                        Point q = (Point) o;
+                        return PointUtil.coincide(p, q, precision);
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * 在最小精度范围内判断点集是否每个点都重合
+     *
+     * @param source 第一个点集
+     * @param target 第二个点集
+     * @return 若两个点集完全重合, 则返回true
+     */
+    public static boolean coincide(Point[] source, final Point[] target) {
+        return coincide(source, target, NumberUtil.MIN_VALUE);
+    }
 }
