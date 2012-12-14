f2c00b0f2fcfc6502b9bf107f19531dd327e71ff
zantesu <dongxin@live.com>
2012-12-14 09:22:05 +0800

    添加由坐标和半径构造圆的方法

--- src/com/fudaowang/common/graph/Round.java
+++ src/com/fudaowang/common/graph/Round.java
@@ -34,6 +34,21 @@ public class Round {
     }
 
     /**
+     * 利用圆心的横坐标和纵坐标以及半径长来构造圆
+     *
+     * @param x      圆心的横坐标
+     * @param y      圆心的纵坐标
+     * @param radius 半径长
+     */
+    public Round(double x, double y, double radius) {
+        if (!NumberUtil.isMoreThanZero(radius)) {
+            throw new IllegalArgumentException("半径长必须大于0");
+        }
+        this.center = new Point(x, y);
+        this.radius = radius;
+    }
+
+    /**
      * 获得点的圆心
      *
      * @return 圆心
