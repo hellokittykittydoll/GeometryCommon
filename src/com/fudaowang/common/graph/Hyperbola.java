109b7504d69a312e8b529ebc42282df94dcfa530
zantesu <dongxin@live.com>
2012-12-17 17:02:37 +0800

    对反比例函数添加原点及其横坐标,纵坐标

--- src/com/fudaowang/common/graph/Hyperbola.java
+++ src/com/fudaowang/common/graph/Hyperbola.java
@@ -12,17 +12,42 @@ import com.fudaowang.common.util.NumberUtil;
  */
 public class Hyperbola {
     private double k;
+    private double x;
+    private double y;
 
     /**
      * 利用系数k构造反比例函数
      *
-     * @param k 系数k
+     * @param k     系数k
+     * @param point 函数的原点
      */
-    public Hyperbola(double k) {
+    public Hyperbola(double k, Point point) {
+        if (NumberUtil.isZero(k)) {
+            throw new IllegalArgumentException("反比例函数的系数k不能为0");
+        }
+        if (point == null) {
+            throw new NullPointerException("原点为null");
+        }
+
+        this.k = k;
+        this.x = point.getX();
+        this.y = point.getY();
+    }
+
+    /**
+     * 构造反比例函数
+     *
+     * @param k 系数k
+     * @param x 原点的横坐标
+     * @param y 原点的纵坐标
+     */
+    public Hyperbola(double k, double x, double y) {
         if (NumberUtil.isZero(k)) {
             throw new IllegalArgumentException("反比例函数的系数k不能为0");
         }
         this.k = k;
+        this.x = x;
+        this.y = y;
     }
 
     /**
@@ -32,6 +57,24 @@ public class Hyperbola {
      */
     public double getK() {
         return k;
+    }
+
+    /**
+     * 获得原点的横坐标
+     *
+     * @return 原点的横坐标
+     */
+    public double getX() {
+        return x;
+    }
+
+    /**
+     * 获得原点的纵坐标
+     *
+     * @return 原点的纵坐标
+     */
+    public double getY() {
+        return y;
     }
 
     public String toString() {
